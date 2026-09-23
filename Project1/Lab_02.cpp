ab624e546d9edf4ec8a00ce128f58a0bc332ace3
ngricenko201-lang <ngricenko201@gmail.com>
2026-09-23 23:44:22 +0300

    second commit

--- Project1/Lab_02.cpp
+++ Project1/Lab_02.cpp
@@ -9,16 +9,16 @@ using namespace std;
 int main()
 {
 	double x; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
+	//double z1; // результат обчислення 1-го виразу
 	double z2; // результат обчислення 2-го виразу
 
 	cout << "x = "; cin >> x;
 	
-	z1 = ((sin(x) + sin(5*x) - sin(3*x)) / (cos(x) - cos(3*x) + cos(5*x)));
+	//z1 = ((sin(x) + sin(5*x) - sin(3*x)) / (cos(x) - cos(3*x) + cos(5*x)));
 	z2 = tan(3*x);
 	
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
+	//cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 	
 	cin.get();
