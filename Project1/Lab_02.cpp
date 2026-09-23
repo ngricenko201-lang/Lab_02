bae3c693786ac4ddd7299181c204278720312bc3
ngricenko201-lang <ngricenko201@gmail.com>
2026-09-23 23:41:48 +0300

    first commit

---
+++ Project1/Lab_02.cpp
@@ -0,0 +1,26 @@
+// Lab_02.cpp
+// < Гриценко Нікіта >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 4
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	double x; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+
+	cout << "x = "; cin >> x;
+	
+	z1 = ((sin(x) + sin(5*x) - sin(3*x)) / (cos(x) - cos(3*x) + cos(5*x)));
+	z2 = tan(3*x);
+	
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	
+	cin.get();
+	return 0;
+}
